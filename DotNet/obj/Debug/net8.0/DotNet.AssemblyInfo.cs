e443d0c53c3550c217be1303c2d4c4b3f31df3a3
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-15 09:26:32 +0100

    uu

--- DotNet/obj/Debug/net8.0/DotNet.AssemblyInfo.cs
+++ DotNet/obj/Debug/net8.0/DotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882f3d710931cb6da8b265d1858f666931f26c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
